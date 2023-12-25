113bdf4e94abb2979e5147d1b397550405c59cc2
sanika391 <116996971+sanika391@users.noreply.github.com>
2023-12-25 15:51:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed('.text', {
+    strings:["Liasion Officer(G20|W20)", "Software Developer", "Frontend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
